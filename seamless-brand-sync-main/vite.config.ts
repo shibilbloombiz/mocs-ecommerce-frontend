3eaa2ce3d20eb97d76f4dd71f929c4528c6daac8
shibil <shibilkp6657@gmail.com>
2026-08-14 13:00:22 +0530

    bug fixed

--- seamless-brand-sync-main/vite.config.ts
+++ seamless-brand-sync-main/vite.config.ts
@@ -18,27 +18,6 @@ export default defineConfig({
       target: "esnext",
       cssCodeSplit: true,
       minify: "esbuild",
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              if (id.includes("react") || id.includes("react-dom")) {
-                return "vendor-react";
-              }
-              if (id.includes("@tanstack")) {
-                return "vendor-tanstack";
-              }
-              if (id.includes("lucide-react")) {
-                return "vendor-icons";
-              }
-              if (id.includes("motion") || id.includes("framer-motion")) {
-                return "vendor-motion";
-              }
-              return "vendor-libs";
-            }
-          },
-        },
-      },
     },
   },
 });
